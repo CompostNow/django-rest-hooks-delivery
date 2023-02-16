0dd24fea7d4e6759ef94775246d9cee2ab6f5997
Dominique Bischof <dbischof@gmail.com>
2023-02-16 17:12:16 -0500

    Update 0001_initial.py
    Django 3.2+ now defaults to BigAutoField for primary keys.

--- rest_hooks_delivery/migrations/0001_initial.py
+++ rest_hooks_delivery/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='FailedHook',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.BigAutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('last_retry', models.DateTimeField(auto_now=True, db_index=True)),
                 ('target', models.URLField(verbose_name='Original target URL', max_length=255, editable=False, db_index=True)),
                 ('event', models.CharField(db_index=True, verbose_name='Event', max_length=64, editable=False, choices=[('customer.created', 'customer.created'), ('customer.deleted', 'customer.deleted'), ('customer.updated', 'customer.updated'), ('invoice.created', 'invoice.created'), ('invoice.deleted', 'invoice.deleted'), ('invoice.updated', 'invoice.updated'), ('plan.created', 'plan.created'), ('plan.deleted', 'plan.deleted'), ('plan.updated', 'plan.updated'), ('proforma.created', 'proforma.created'), ('proforma.deleted', 'proforma.deleted'), ('proforma.updated', 'proforma.updated'), ('provider.created', 'provider.created'), ('provider.deleted', 'provider.deleted'), ('provider.updated', 'provider.updated'), ('subscription.created', 'subscription.created'), ('subscription.deleted', 'subscription.deleted'), ('subscription.updated', 'subscription.updated')])),
